29dfb0bb8e4b5e8a88355a9b58a32e42017b8764
nesfujiki <97644652+nesfujiki@users.noreply.github.com>
2022-06-08 23:42:46 +0900

    テンプレート文字列まで

--- src/index.js
+++ src/index.js
@@ -1,10 +1,58 @@
-import "./styles.css";
+/**
+ * const,let等の変数宣言
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = "var変数";
+// console.log(vall);
+// //var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(vall);
+// //var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(vall);
+
+// let val2 = "let変数";
+// console.log(val2);
+// //letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+// //let再ggggg
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+// //const変数は上書き不可
+// val3 = "const変数を上書き";
+// //const変数は再宣言不可能
+// const val3 = "const変数を再宣言";
+
+// //constで定義したオブジェクトはプロパティの変更が可能。オブジェクトはconstで宣言する
+// const val4 = {
+//   name: "yasu",
+//   age: 24
+// };
+// val4.name = "やす";
+// val4.address = "Fukuoka";
+// console.log(val4);
+
+// // //constで定義した配列はプロパティの変更が可能。配列はconstで宣言する
+// const val5 = ['dog', 'cat'];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+
+const name = "yasu";
+const age = 24;
+//「私の名前はやすです。年齢は24です。」
+
+//従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message1);
+
+//テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message2);
